d062c65d6a258eec17d3bd8fb6b336787884a2d5
sujanthapa2k79 <sujanthapa52@gmail.com>
2022-07-24 08:43:56 +0545

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RootApp());
+}
+
+class RootApp extends StatelessWidget {
+  const Myapp({key? key}) : super(key: key);
+
+
+
+class MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/root_app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RootApp());
+}
+
+class RootApp extends StatelessWidget {
+  final age = 10;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.orange,
+          appBar: AppBar(
+            title: Text.rich(
+              TextSpan(
+                text: 'First',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Project',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: SafeArea(
+            child: Text('I love Nepal'),
+          ),
+        ));
+  }
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
